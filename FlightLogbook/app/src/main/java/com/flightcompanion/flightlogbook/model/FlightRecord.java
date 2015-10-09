4965ba1d02a6c4f0f35a7dd7e4ebbc56d723026d
László Dezsényi <laszlo.dezsenyi@prezi.com>
2015-10-08 23:02:17 -0700

    Introduce flight record

---
+++ FlightLogbook/app/src/main/java/com/flightcompanion/flightlogbook/model/FlightRecord.java
@@ -0,0 +1,76 @@
+package com.flightcompanion.flightlogbook.model;
+
+import java.util.Date;
+
+public class FlightRecord {
+
+	private Date date;
+	private Long duration;
+	private String plane;
+	private String location;
+	private String task;
+	private String comment;
+
+	public FlightRecord(Date date, Long duration, String plane, String location, String task, String comment) {
+		this.date = date;
+		this.duration = duration;
+		this.plane = plane;
+		this.location = location;
+		this.task = task;
+		this.comment = comment;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public Long getDuration() {
+		return duration;
+	}
+
+	public String getPlane() {
+		return plane;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public String getTask() {
+		return task;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public static class Builder {
+		private Date date = new Date();
+		private Long duration;
+		private String plane = "";
+		private String location = "";
+		private String task = "";
+		private String comment = "";
+
+		public Builder(Date date, String plane, long duration, String location) {
+			this.date = date;
+			this.plane = plane;
+			this.duration = duration;
+			this.location = location;
+		}
+
+		public Builder task(String task) {
+			this.task = task;
+			return this;
+		}
+
+		public Builder comment(String comment) {
+			this.comment = comment;
+			return this;
+		}
+
+		public FlightRecord build() {
+			return new FlightRecord(date, duration, plane, location, task, comment);
+		}
+	}
+}
