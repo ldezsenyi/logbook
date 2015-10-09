16d6655e414e4c466e4c49849bcd56ac44eda0f1
László Dezsényi <laszlo.dezsenyi@prezi.com>
2015-10-08 23:06:52 -0700

    Show flight list fragment

--- FlightLogbook/app/src/main/java/com/flightcompanion/flightlogbook/MainActivity.java
+++ FlightLogbook/app/src/main/java/com/flightcompanion/flightlogbook/MainActivity.java
@@ -1,9 +1,12 @@
 package com.flightcompanion.flightlogbook;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.flightcompanion.flightlogbook.list.FlightListFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,23 +14,26 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		showFlightList(R.id.main_content_container);
+	}
+
+	private void showFlightList(int containerViewId) {
+		FlightListFragment flightListFragment = FlightListFragment.newInstance();
+		FragmentManager fragmentManager = getSupportFragmentManager();
+		FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+		fragmentTransaction.replace(containerViewId,flightListFragment, FlightListFragment.TAG).commit();
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.menu_main, menu);
 		return true;
 	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
 
-		//noinspection SimplifiableIfStatement
 		if (id == R.id.action_settings) {
 			return true;
 		}
